0c1d159b90076a8949b61a61d857d4cdfcc7c03e
tudormoldovan17 <infiniteblazexd@yahoo.com>
2023-04-15 13:55:34 +0300

    yeah lets try cities show all 2

--- react_frontend/src/App.tsx
+++ react_frontend/src/App.tsx
@@ -10,21 +10,21 @@ function App() {
 
   return (
       <React.Fragment>
-      <CitiesShowAll></CitiesShowAll>
-    <div className="App">
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <CitiesShowAll/>
+      <div className="App">
+          <h1>Vite + React</h1>
+          <div className="card">
+            <button onClick={() => setCount((count) => count + 1)}>
+              count is {count}
+            </button>
+            <p>
+              Edit <code>src/App.tsx</code> and save to test HMR
+            </p>
+          </div>
+          <p className="read-the-docs">
+            Click on the Vite and React logos to learn more
+          </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
       </React.Fragment>
   )
 }
