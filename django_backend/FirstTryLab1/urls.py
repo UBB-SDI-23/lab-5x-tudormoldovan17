0137e7dcb224d09a47d7e016871ccfbaf1bce1e6
tudormoldovan17 <infiniteblazexd@yahoo.com>
2023-04-15 15:19:55 +0300

    swagger cica

--- django_backend/FirstTryLab1/urls.py
+++ django_backend/FirstTryLab1/urls.py
@@ -14,9 +14,24 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Worldwide API",
+        default_version='v1',
+        description="Welcome to Worldwide app",
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
+    re_path(r'^doc(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('doc/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),  # <-- Here
     path('admin/', admin.site.urls),
     path('api/', include('api.urls'))
 ]
