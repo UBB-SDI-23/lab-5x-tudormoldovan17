9dcd134db6d572347d1bfa6407c5c2e7b4141fb1
tudormoldovan17 <infiniteblazexd@yahoo.com>
2023-04-19 17:06:43 +0300

    changed backend api url

--- react_frontend/src/constants.ts
+++ react_frontend/src/constants.ts
@@ -1,5 +1,5 @@
 
 
-export const BACKEND_API_URL = "http://34.154.231.138/api";
+export const BACKEND_API_URL = "http://34.154.118.191/api";
 
     /*process.env.NODE_ENV === "development" ? DEV_BACKEND_API_URL : PROD_BACKEND_API_URL;*/
