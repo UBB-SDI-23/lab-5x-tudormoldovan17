066cbadec5be11e538555666c65988c270ab0aa2
tudormoldovan17 <infiniteblazexd@yahoo.com>
2023-04-21 18:24:10 +0300

    https

--- react_frontend/src/constants.ts
+++ react_frontend/src/constants.ts
@@ -1,5 +1,5 @@
 
 
-export const BACKEND_API_URL = "https://34.154.118.191/api";
+export const BACKEND_API_URL = "http://34.154.118.191/api";
 
     /*process.env.NODE_ENV === "development" ? DEV_BACKEND_API_URL : PROD_BACKEND_API_URL;*/
