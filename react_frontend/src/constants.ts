e41e52fafcd284a3a9546264cca3507f2b7af042
tudormoldovan17 <infiniteblazexd@yahoo.com>
2023-04-25 14:52:35 +0300

    dada

--- react_frontend/src/constants.ts
+++ react_frontend/src/constants.ts
@@ -1,5 +1,5 @@
 
 
-export const BACKEND_API_URL = "http://34.154.231.138/api";
+export const BACKEND_API_URL = "http://34.154.19.229/api";
 
     /*process.env.NODE_ENV === "development" ? DEV_BACKEND_API_URL : PROD_BACKEND_API_URL;*/
